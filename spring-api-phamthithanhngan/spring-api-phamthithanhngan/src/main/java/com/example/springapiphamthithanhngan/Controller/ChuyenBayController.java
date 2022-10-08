898ca08ba7b25671fd2d9925931dac4e4736de31
NguyenThiThanhNgan <thanhngan1262018@gmail.com>
2022-10-08 14:32:57 +0700

    Hoàn thành câu 1

---
+++ spring-api-phamthithanhngan/spring-api-phamthithanhngan/src/main/java/com/example/springapiphamthithanhngan/Controller/ChuyenBayController.java
@@ -0,0 +1,28 @@
+package com.example.springapiphamthithanhngan.Controller;
+
+import com.example.springapiphamthithanhngan.Entity.ChuyenBay;
+import com.example.springapiphamthithanhngan.Service.ChuyenBayRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class ChuyenBayController {
+    @Autowired
+    ChuyenBayRepository chuyenBayRepository;
+    //Câu 1
+    @RequestMapping(value = "/timchuyenbayboigaden/{gaden}", method = RequestMethod.GET)
+    public List<ChuyenBay> timChuyenBayBoiGaDen(@PathVariable("gaden") String gaDen){
+        List<ChuyenBay> chuyenBays = chuyenBayRepository.findChuyenBayByGaDen(gaDen);
+        if(chuyenBays == null){
+            ResponseEntity.notFound().build();
+        }
+        return chuyenBays;
+    }
+
+}
