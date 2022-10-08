1cbfc35d5ff97de4db2a815a0898c2ff8c076ee3
NguyenThiThanhNgan <thanhngan1262018@gmail.com>
2022-10-08 12:48:33 +0700

    Tao project

---
+++ spring-api-phamthithanhngan/spring-api-phamthithanhngan/src/test/java/com/example/springapiphamthithanhngan/SpringApiPhamthithanhnganApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springapiphamthithanhngan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringApiPhamthithanhnganApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
